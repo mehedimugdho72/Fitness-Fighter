c96b876490b04393790262960988810df0013246
MEHWDI HASAN MUGDHO <mehedimugdho72@gmail.com>
2022-04-21 05:59:26 +0600

    7th commit

--- src/Pages/firebase.init.js
+++ src/Pages/firebase.init.js
@@ -1,17 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAZxcXiQVt7hui-hHASDmO-tbl9xyVcHPE",
-    authDomain: "power-zone-d77bd.firebaseapp.com",
-    projectId: "power-zone-d77bd",
-    storageBucket: "power-zone-d77bd.appspot.com",
-    messagingSenderId: "605369002153",
-    appId: "1:605369002153:web:e73713a1a2f33b4a5d21b8"
+  apiKey: "AIzaSyC1QELgIPjUZD-092HY7_JZjcaX3vHi0hs",
+  authDomain: "fitness-fighter-c557a.firebaseapp.com",
+  projectId: "fitness-fighter-c557a",
+  storageBucket: "fitness-fighter-c557a.appspot.com",
+  messagingSenderId: "1065701716617",
+  appId: "1:1065701716617:web:f1e110fdd69b2071b08c58"
 };
 
 // Initialize Firebase
